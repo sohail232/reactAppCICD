ab26b27e1afe6ea5741a277b4af52c43870df709
sohail232 <90613331+sohail232@users.noreply.github.com>
2025-07-15 00:52:07 +0500

    changes to be test

--- src/App.js
+++ src/App.js
@@ -7,17 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. now check for reload
-          first app to test CI/CD pipeline.
+          Edit <code>src/App.js</code> and save to reload. need to test it
+          again.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
