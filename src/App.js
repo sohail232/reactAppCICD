f7bbbf134d2bce29294eb476dcca977ad8082d65
sohail232 <90613331+sohail232@users.noreply.github.com>
2025-07-15 00:34:38 +0500

    test CI/CD

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. now check for reload
+          first app to test CI/CD pipeline.
         </p>
         <a
           className="App-link"
